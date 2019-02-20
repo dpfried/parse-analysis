135a92f829ae66cb4b7025b777c213234fefbad5
Nikita Kitaev <nikitakit@gmail.com>
2019-02-20 11:14:24 -0800

    Download Genia as part of processing

--- corpora/genia/build_corpus.sh
+++ corpora/genia/build_corpus.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+DATA_URL="http://bllip.cs.brown.edu/download/genia1.0-division-rel1.tar.gz"
+
 SCRIPT_DIR="../../scripts"
 STRIP_ROOT=${SCRIPT_DIR}/strip_root.py
 STRIP_FUNCTIONAL=${SCRIPT_DIR}/strip_functional.py
 ENSURE_TOP=${SCRIPT_DIR}/ensure_top.py
 
+if [ ! -e genia1.0-division-rel1.tar.gz ]; then
+    wget ${DATA_URL} genia1.0-division-rel1.tar.gz
+fi
+
+# This overrides any previous extracted copy
+tar xf genia1.0-division-rel1.tar.gz
+
 for SPLIT in train dev test
 do
-  RAW=${SPLIT}.trees
+  RAW=genia-dist/division/${SPLIT}.trees
   STRIPPED=${SPLIT}.stripped
-  PROCESSED=${SPLIT}.processed
 
-  python $STRIP_ROOT --symbol S1 --must_have < $RAW | python $STRIP_ROOT --symbol S --must_have | python $ENSURE_TOP > $STRIPPED  
+  python $STRIP_ROOT --symbol S1 --must_have < $RAW | python $STRIP_ROOT --symbol S --must_have | python $ENSURE_TOP > $STRIPPED
 done
