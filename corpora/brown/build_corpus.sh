6f8428c16514b92174903e76803e9864709602c4
Nikita Kitaev <nikitakit@gmail.com>
2019-02-20 14:32:01 -0800

    Generate files for individual sections of Brown

--- corpora/brown/build_corpus.sh
+++ corpora/brown/build_corpus.sh
@@ -14,3 +14,8 @@ python $STRIP_ROOT --symbol '' < $RAW | python $ENSURE_TOP | python $STRIP_FUNCT
 python partition_brown.py --split 'train' <$STRIPPED >train.stripped
 python partition_brown.py --split 'test' <$STRIPPED >test.stripped
 python partition_brown.py --split 'test40' <$STRIPPED >test40.stripped
+
+for SECTION in cf cg ck cl cm cn cp cr
+do
+    python split_brown_by_section.py --section $SECTION <$STRIPPED >${SECTION}.stripped
+done
